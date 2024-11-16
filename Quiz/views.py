57dfa8e12612aa6874856195adfa15c02f57b31f
MJTech46 <140804026+MJTech46@users.noreply.github.com>
2024-11-17 00:35:39 +0530

    forced sign-in: do it ;)

--- Quiz/views.py
+++ Quiz/views.py
@@ -1,21 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from Account.models import CustomUser
 
 def login(request: HttpRequest) -> HttpResponse:
-    return render(request,"Quiz/login.html")
+    if request.method == "GET":
+        return render(request,"Quiz/login.html")
+    
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(username=username, password=password)
+        if user:
+            auth_login(request, user)
+            return redirect("home")
+        else:
+            return render(request,"Quiz/login.html", {"message":"Wrong credentials. please check!"})
 
 def signup(request: HttpRequest) -> HttpResponse:
-    return render(request,"Quiz/signup.html")
+    if request.method == "GET":
+        return render(request,"Quiz/signup.html")
+    
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        re_password = request.POST.get("re-password")
+        try:
+            if  password == re_password:
+                CustomUser.objects.create_user(username=username, password=password)
+                return redirect("login")
+            else:
+                return render(request,"Quiz/signup.html", {"message":"Password doesn't mach. please check!"})
+        except Exception as e:
+                return render(request,"Quiz/signup.html", {"message":"User already exist !"})
 
+
+@login_required(login_url="login")
+def logout(request: HttpRequest) -> HttpResponse:
+    auth_logout(request)
+    return redirect("login")
+
+@login_required(login_url="login")
 def home(request: HttpRequest) -> HttpResponse:
     context = {
         "admin":request.user.is_superuser
     }
     return render(request,"Quiz/index.html", context)
 
+
+@login_required(login_url="login")
 def redeem(request: HttpRequest) -> HttpResponse:
     return render(request,"Quiz/redeem.html")
 
+
+@login_required(login_url="login")
 def quiz(request: HttpRequest) -> HttpResponse:
     return render(request,"Quiz/quiz.html")
 
